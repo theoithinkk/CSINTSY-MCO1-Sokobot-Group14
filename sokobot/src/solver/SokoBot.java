71cc6b47ed45ea6391a218a647722aef68524b0a
m-fides <maria_fides_bancoro@dlsu.edu.ph>
2026-06-18 05:16:36 +0800

    Update SokoBot.java

--- sokobot/src/solver/SokoBot.java
+++ sokobot/src/solver/SokoBot.java
@@ -2,6 +2,34 @@ package solver;
 
 public class SokoBot {
 
+    /** Maximum width of puzzle grid. */
+    private int width;
+    
+    /** Maximum height of puzzle grid. */
+    private int height;
+    
+    /** 2D Matrix that stores the static walls of the current sokoban puzzle. 
+     *    - True indicates a wall (unpassable space). Otherwise,
+     *    - False indicates a passable space, a box, the player bot, or a goal. 
+     */
+    private boolean[][] walls;
+    
+    /** 2D Matrix mapping target positions for the boxes. 
+     *    - True indicates a goal is located at the grid coordinates.
+     *    - False indicates a non-goal space.
+    */
+    private boolean[][] goals;
+
+    /** 2D Matrix that stores the dead squares of the current sokoban puzzle. 
+     *    - True indicates that the cell is a non-goal space that would make the puzzle unsolvable if boxes are pushed onto them.
+     *    - False indicates that the cell is either safe for a box or is a goal. 
+    */
+    private boolean[][] deadSquares;
+
+    /** 3D Distance Matrix for Min-Push Heuristic calculation.  */
+    // private int[][][] distanceTable;
+
+
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     /*
      * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
@@ -18,5 +46,82 @@ public class SokoBot {
     }
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
+
+  /** ===========================================================================================
+   * PART 1: BOARD REPRESENTATION AND PREPROCESSING 
+   *    - initialize(): gets how the board looks like 
+   *    - computeDeadSquares(): marks non goal corner cells as dead squares cus any box pushed into those cells can never be moved into a goal
+   *    - computeBoxGoalDistances(): computes backwards from a goal that results in a table that has a lower bound on how many pushes a box needs to reach each goal
+   * ============================================================================================*/
+
+  private void initialize(int width, int height, char[][] mapData, char[][] itemsData) {
+
+      this.width = width;
+      this.height = height;
+
+      // instantiate the walls and goals matrices based on the map data
+      this.walls = new boolean[height][width];
+      this.goals = new boolean[height][width];
+
+      // main loop for puzzle board parsing
+      for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                char staticTile = mapData[r][c];
+                
+                //case 1: wall detection (set walls[r][c] to true if there is a wall at (r, c))
+                if (staticTile == '#') {
+                    this.walls[r][c] = true;
+                } 
+                //case 2: goal detection (set goals[r][c] to true if there is a goal at (r, c))
+                else if (staticTile == '.') {
+                    this.goals[r][c] = true;
+                }
+            }
+      }
+
+      computeDeadSquares(); //based on the extracted walls and goals, compute the dead squares of the puzzle.
+      computeBoxGoalDistances(); //based on the extracted walls and goals, compute the distance table for the min-push heuristic.
+
+  }
+
+  private void computeDeadSquares() {
+
+    this.deadSquares = new boolean[height][width];
+
+    //Case 1: Dead Corner Detection (mark every corner that is a non-goal square as a dead square, since any box pushed into that cell can never be moved into a goal.)
+    for (int r = 0; r < height; r++) {
+        for (int c = 0; c < width; c++) {
+            
+            // Exclude coordinates holding blocking structures or valid goals
+            if (walls[r][c] || goals[r][c]) {
+                continue;
+            }
+            
+            // Checking of adjacent cells and determining whether the current cell is a corner (has 2 adjacent walls that are perpendicular to each other)
+            boolean wallNorth = (r - 1 >= 0) && walls[r - 1][c];
+            boolean wallSouth = (r + 1 < height) && walls[r + 1][c];
+            boolean wallWest  = (c - 1 >= 0) && walls[r][c - 1];
+            boolean wallEast  = (c + 1 < width) && walls[r][c + 1];
+            
+            // Mark the tile as a dead square if it is a corner (has 2 adjacent walls that are perpendicular to each other)
+            if ((wallNorth && wallEast) || 
+                (wallEast && wallSouth) || 
+                (wallSouth && wallWest) || 
+                (wallWest && wallNorth)) {
+                
+                this.deadSquares[r][c] = true; 
+            }
+        }
+    }
+
+  }
+
+  private void computeBoxGoalDistances() {
+
+  }
+
+
+
+
 
 }
